01cf9e80715c2005db3b3e87efbaca0529c54edf
Deepak Tripathi <deepak.tripathi@tala.co>
2020-05-28 18:02:32 -0700

    DR->Snowflake WIP

--- app/Handler/DrmInfoHandler.scala
+++ app/Handler/DrmInfoHandler.scala
@@ -1,19 +1,33 @@
 package Handler
 
+import java.sql.Timestamp
 
-import models.AndroidRequest
+import models.{AndroidRequest, DrmInfo}
 import play.api.libs.json.{JsObject, JsValue, Json}
 import org.apache.avro.Schema
 import org.apache.avro.Schema.Parser
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerRecord, RecordMetadata}
+import play.api.Logger
 
-
+import scala.concurrent.Promise
 import scala.io.Source
 import scala.collection.JavaConverters._
+import net.liftweb.json._
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.sql.{PreparedStatement, Timestamp, Types}
+import java.util.Calendar
+
+
 
 class DrmInfoHandler(request: AndroidRequest) extends Handler {
 
+
+  val accessLogger: Logger = Logger(this.getClass)
+  implicit val formats = net.liftweb.json.DefaultFormats
 
   override protected val androidRequest: AndroidRequest = request
 
@@ -21,7 +35,18 @@ class DrmInfoHandler(request: AndroidRequest) extends Handler {
     transferToSnowFlake = true
   )
 
+  def getCurrentdateTimeStamp: Timestamp ={
+    val today:java.util.Date = Calendar.getInstance.getTime
+    val timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val now:String = timeFormat.format(today)
+    val re = java.sql.Timestamp.valueOf(now)
+    re
+  }
+
   override def convertToJson(data: JsValue): Option[JsObject] = {
+
+
+
 
 
     Some(JsObject(Seq(
@@ -39,6 +64,18 @@ class DrmInfoHandler(request: AndroidRequest) extends Handler {
   override def avero(data: JsValue): Option[JsObject] = {
 
 
+    val drmInfoModel = DrmInfo(
+      None,
+      androidRequest.personId,
+      androidRequest.deviceId,
+      (data \ "common_pssh_device_id").asOpt[String],
+      (data \ "clearkey_device_id").asOpt[String],
+      (data \ "playready_device_id").asOpt[String],
+      (data \ "widevine_device_id").asOpt[String],
+      getCurrentdateTimeStamp
+
+    )
+
     val props: Map[String, AnyRef] = Map(
       "bootstrap.servers" -> "localhost:9092",
       "group.id" -> "CountryCounter",
@@ -47,26 +84,45 @@ class DrmInfoHandler(request: AndroidRequest) extends Handler {
       "schema.registry.url" -> "http://localhost:8081"
 
     )
-    val schema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/test.avsc")).mkString)
+
+
+    val drmInfoSchema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/DrmInfo.avsc")).mkString)
+
+
+
+    val drmInfoRecord: GenericRecord = new GenericData.Record(drmInfoSchema)
+    drmInfoRecord.put("personId" , drmInfoModel.personId)
+    drmInfoRecord.put("deviceId" ,  drmInfoModel.deviceId)
+    drmInfoRecord.put("commonPsshDeviceId" , drmInfoModel.commonPsshDeviceId.orNull)
+    drmInfoRecord.put("clearkeyDeviceId" ,  drmInfoModel.clearkeyDeviceId.orNull)
+    drmInfoRecord.put("playreadyDeviceId" , drmInfoModel.playreadyDeviceId.orNull)
+    drmInfoRecord.put("widevineDeviceId" , drmInfoModel.widevineDeviceId.orNull)
+    drmInfoRecord.put("serverTime" , getCurrentdateTimeStamp)
+
+
+
+
+
+    //val schema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/test.avsc")).mkString)
 
     // Create avro generic record object
-    val genericUser: GenericRecord = new GenericData.Record(schema)
+    //val genericUser: GenericRecord = new GenericData.Record(schema)
 
     //Put data in that generic record
-    genericUser.put("id", 1)
-    genericUser.put("name", "test_name")
-    genericUser.put("email", null)
+//    genericUser.put("id", 1)
+//    genericUser.put("name", "test_name")
+//    genericUser.put("email", null)
+//    genericUser.put("address", "address")
 
     val producer = new KafkaProducer[Int, GenericRecord](props.asJava)
 
-    val record = new ProducerRecord("test", 1, genericUser)
+    val record = new ProducerRecord("DrmInfo", 1, drmInfoRecord)
+    val promise = Promise[RecordMetadata]()
 
-    producer.send(record, new Callback() {
-      override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+    producer.send(record, producerCallback(promise))
+    //val v = promise.future
+    //accessLogger.info("test")
 
-
-      }
-    })
 
     //    // Serialize generic record into byte array
     //    val writer = new SpecificDatumWriter[GenericRecord](schema)
@@ -100,6 +156,30 @@ class DrmInfoHandler(request: AndroidRequest) extends Handler {
       "server_time" -> Json.toJson(System.currentTimeMillis() / 1000), // unix timestamp
     )))
 
+
+  }
+
+  private def producerCallback(promise: Promise[RecordMetadata]) : Callback = {
+
+    new Callback {
+      override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+
+        if (exception == null) {
+          accessLogger.info("offset - " + metadata.offset())
+          accessLogger.info("topic - " + metadata.topic())
+          accessLogger.info("partition - " + metadata.partition())
+        }
+        else {
+          accessLogger.error(exception.printStackTrace().toString)
+        }
+
+
+
+
+
+        //promise.complete(result)
+      }
+    }
 
   }
 
--- build.sbt
+++ build.sbt
@@ -1,31 +1,25 @@
 name := "Data_Receiver_Snowflake"
- 
-version := "1.0" 
-      
-lazy val `Data_Receiver_Snowflake` = (project in file(".")).enablePlugins(PlayScala)
-
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
-      
-resolvers += "Akka Snapshot Repository" at "https://repo.akka.io/snapshots/"
-
-resolvers += "Confluent" at "http://packages.confluent.io/maven/"
-      
+version := "1.0"
 scalaVersion := "2.12.2"
 
-libraryDependencies ++= Seq( ws, javaWs, jdbc , ehcache , specs2 % Test , guice )
-libraryDependencies += "com.typesafe.play" %% "play-json" % "2.8.1"
-// https://mvnrepository.com/artifact/javax.ws.rs/javax.ws.rs-api
-libraryDependencies += "javax.ws.rs" % "jsr311-api" % "0.10"
-libraryDependencies += "org.apache.avro" % "avro" % "1.7.7"
-libraryDependencies += "org.apache.kafka" %% "kafka" % "2.1.0"
-// https://mvnrepository.com/artifact/io.confluent/kafka-avro-serializer
-libraryDependencies += "io.confluent" % "kafka-avro-serializer" % "3.3.1"
+lazy val `Data_Receiver_Snowflake` = (project in file(".")).enablePlugins(PlayScala)
 
+resolvers ++= Seq( "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases",
+ "Akka Snapshot Repository" at "https://repo.akka.io/snapshots/" ,
+"Confluent" at "http://packages.confluent.io/maven/" )
 
+libraryDependencies ++= Seq( ws, javaWs, jdbc , ehcache , specs2 % Test , guice
+, "com.typesafe.play" %% "play-json" % "2.8.1"
+, "org.apache.avro" % "avro" % "1.7.7"
+, "org.apache.kafka" %% "kafka" % "2.1.0"
+ // https://mvnrepository.com/artifact/javax.ws.rs/javax.ws.rs-api
+, "javax.ws.rs" % "jsr311-api" % "0.10"
+ // https://mvnrepository.com/artifact/io.confluent/kafka-avro-serializer
+, "io.confluent" % "kafka-avro-serializer" % "3.3.1"
+ // https://mvnrepository.com/artifact/net.liftweb/lift-json
+, "net.liftweb" %% "lift-json" % "3.4.1"
+)
 
-
-
-
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
+//unmanagedResourceDirectories in Test +=  baseDirectory ( _ /"target/web/public/test" )
 
       
